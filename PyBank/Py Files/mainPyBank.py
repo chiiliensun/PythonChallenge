bf75bb5bb2864aa9d623e1f152e9139fdc09619a
Chia <chiilien.sun@gmail.com>
2021-01-11 19:44:37 -0800

    finished mostly, missing one bullet but will try later

--- PyBank/Py Files/mainPyBank.py
+++ PyBank/Py Files/mainPyBank.py
@@ -20,6 +20,7 @@ csvpath = os.path.join("PyBank/Resources", "budget_data.csv")
 # set the lists
 monthcount = []
 netprofit = []
+file_to_output = os.path.join("pyBank.txt")
 
 # open the cvs file to read the data
 with open(csvpath, newline='') as csvfile:
@@ -30,14 +31,40 @@ with open(csvpath, newline='') as csvfile:
         monthcount.append(row[0])
         netprofit.append(int(row[1]))
 
-#calculation for total months
+# calculation for total months
 total_months = (len(monthcount))
 
-#calculation for net total amount of "profit/losses" over entire period
-net_total = sum(netprofit)
+# calculation for net total amount of "profit/losses" over entire period
+netprofit_total = sum(netprofit)
+
+# calculate the changes in "profit/losses" over entire period, start with $ total 
+
+#highest and lowest change
+highest_change = max(netprofit)
+lowest_change = min(netprofit)
+
+#highest and lowest month
+highest_month = netprofit.index(highest_change)
+lowest_month = netprofit.index(lowest_change)
+
+#now bestestest month
+best = monthcount[highest_month]
+worst = monthcount[lowest_month]
 
 # check where I'm at, I have no idea what I was trying earlier
 #I made it way too hard, success for total amount!
+print("Financial Analysis")
+print("-----------------------------------------------------")
 print(f'Total Months: {total_months}')
-print(f'Total: ${net_total}')
+print(f'Total: ${netprofit_total}')
+print(f'Greatest Increase in Profits: {best} (${highest_change})')
+print(f'Greatest Decrease in Losses: {worst} (${lowest_change})')
 
+pyBank_output = os.path.join(".", "pyBank_output.txt")
+with open(pyBank_output, "w") as new:
+    new.write("Financial Analysis")
+    new.write("-----------------------------------------------------")
+    new.write(f'Total Months: {total_months}')
+    new.write(f'Total: ${netprofit_total}')
+    new.write(f'Greatest Increase in Profits: {best} (${highest_change})')
+    new.write(f'Greatest Decrease in Losses: {worst} (${lowest_change})')
