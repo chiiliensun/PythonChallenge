c552f14d39f612555002e081ba772dd7a28b47b5
Chia <chiilien.sun@gmail.com>
2021-01-11 16:15:22 -0800

    One bullet down, Total months and confirmed it printed correctly

--- PyBank/Py Files/main.py
+++ PyBank/Py Files/main.py
@@ -1,17 +1,32 @@
 #importing the modules
 import os
 import csv
-# print("---My Prin---")
-# print(os.getcwd()) where is my directory at?
-# print("--end--")
 
-#Didn't understand the path, found that needing to add where in PythonChallenge folder that would find the Resources folder
+# completing the path C:\Users\sun_c\Documents\Bootcamp\PythonChallenge\PythonChallenge\PyBank\Resources\budget_data.csvr
 csvpath = os.path.join('PyBank/Resources', 'budget_data.csv')
 
-#ensuring that my csv file is found and imported
-#finally success! Missed the PyBank/Resources
+# make the lists
+total = []
+months = []
+month_change = []
+
+# #stupic calculations
+# def average (numbers):
+#     total = 0.0
+#     for number in numbers:
+#         total += number
+#     average_month = total/len(numbers)
+#     return average_month
+
+# open the cvs file to read the data
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-    print(csv_header)
+    # print(csv_header)
+    for row in csvreader:
+        months.append(row[0])
+        total.append(row[1])
+        month_change.append(int(row[1]))
+
+print(f'Total Months: {len(months)}')
 
--- PyBank/Py Files/mainPyBank.py
+++ PyBank/Py Files/mainPyBank.py
@@ -1,17 +1,38 @@
-# Python Day 2, 08-Ins_ReadCSV. Need to import the os module that will allow the creation of file paths across operating systems
+# Need to import the os module that will allow the creation of file paths across operating systems
 # Import for reading csv file, for budget.data.csv
 import os
 import csv
 
+# print("---My Prin---")
+# print(os.getcwd()) where is my directory at?
+# print("--end--")
+# #finally success! Missed the PyBank/Resources
+
 # completing the path C:\Users\sun_c\Documents\Bootcamp\PythonChallenge\PythonChallenge\PyBank\Resources\budget_data.csv
 csvpath = os.path.join("PyBank/Resources", "budget_data.csv") 
 
-# testing and plain reading of CSV files
+# testing and plain reading of CSV files, will be using later but need to parcel it out
+# with open(csvpath, newline='') as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter=",")
+#     csv_header = next(csvreader)
+#     print(csv_header) 
+
+# set the lists
+monthcount = []
+netchange = []
+
+# open the cvs file to read the data
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-    print(csv_header) 
-# 
-# 
-# 
-# 
+    # don't need the print(csv_header), but loop through reading each row
+    for row in csvreader:
+        monthcount.append(row[0])
+        netchange.append(int(row[1]))
+
+#calculation for total months
+total_months = (len(monthcount))
+
+# check where I'm at
+print(f'Total Months: {total_months}')
+
